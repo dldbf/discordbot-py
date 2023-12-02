477410db2d8e03d4039b6292b45412a6b76f945f
dldbf <84608881+dldbf@users.noreply.github.com>
2023-12-03 01:05:59 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -585,8 +585,10 @@ async def on_message(message):
         await message.channel.send('https://cdn.discordapp.com/attachments/969220566567489566/1078932702255923260/Sanmas.gif')
         
 @client.event
-async def on_reaction_add():
-    if reaction.emoji == "❌":
+async def on_reaction_add(reaction, user):
+    if user.bot == 1: #봇이면 패스
+        return None
+    if str(reaction.emoji) == "❌":
         await reaction.channel.message.delete()
         
         
