7cf5c5387da043206b59769d5e3a549c441481a3
dldbf <84608881+dldbf@users.noreply.github.com>
2023-12-06 17:29:15 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -584,7 +584,7 @@ async def on_message(message):
     if message.content.startswith(f'{PREFIX}정샌찬'):
         await message.channel.send('https://cdn.discordapp.com/attachments/969220566567489566/1078932702255923260/Sanmas.gif')
 
-@client.command()
+@bot.command()
 async def on_reaction_add(reaction, user):
     if user.bot == 1: #봇이면 패스
         return None
