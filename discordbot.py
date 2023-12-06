d9ded917e94d60b9cd87b7d20e9241911ff0f888
dldbf <84608881+dldbf@users.noreply.github.com>
2023-12-06 17:20:39 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -589,11 +589,11 @@ async def on_reaction_add(reaction, user):
     if user.bot == 1: #봇이면 패스
         return None
     i = ctx.message.author.guild_permissions.administrator
-        if i is True:
-             if str(reaction.emoji) == "❌":
-                await reaction.message.channel.send("```cs\n해당 자캐가 '탈락'하였습니다\n24시간 후 해당 자캐가 삭제될 예정입니다\n자캐를 읽기 싫으시다면 지금 복사해두세요!", reference=message)
-                await asyncio.sleep(86400)
-                await reaction.message.delete()
+    if i is True:  
+        if str(reaction.emoji) == "❌":
+            await reaction.message.channel.send("```cs\n해당 자캐가 '탈락'하였습니다\n24시간 후 해당 자캐가 삭제될 예정입니다\n자캐를 읽기 싫으시다면 지금 복사해두세요!", reference=message)
+            await asyncio.sleep(86400)
+            await reaction.message.delete()
 try:
     client.run(TOKEN)
 except discord.errors.LoginFailure as e:
